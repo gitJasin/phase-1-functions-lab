a22dfa5caea0f312046e7f76e580b1701abbb7da
Jason Stewart <jaysin.stewart@gmail.com>
2023-03-15 12:44:53 -0700

    Completed p1 fucntions lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const hqLocation = 42;
+
+function distanceFromHqInBlocks(someValue) {
+    if (someValue >= hqLocation) {
+      return someValue -  hqLocation;
+    } 
+    else {
+      return hqLocation - someValue;
+    }
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+      return (destination - start) * 264
+    } 
+    else {
+      return (start - destination) * 264
+    }
+  }
+
+  function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+      return distanceFromHqInFeet(start, destination) - 264;
+    } 
+    else if (distanceTravelledInFeet(start, destination) >= 400 && (distanceTravelledInFeet(start, destination) <= 2000)) {
+      return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    } 
+    else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+      return 25;
+    }
+    else if (distanceTravelledInFeet(start, destination) >= 2500) {
+      return 'cannot travel that far';
+    }
+  }
